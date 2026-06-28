ab75e4adb4ae514419806739bfd1e549587c0280
Mauro Garavello <mauro.garavello@unimib.it>
2026-06-28 19:02:14 +0200

    upwind and Lax_friedrichs

--- Python/lib/finite_volume_methods.py
+++ Python/lib/finite_volume_methods.py
@@ -6,137 +6,150 @@ with various numerical flux functions.
 """
 
 import numpy as np
-from typing import Callable
 from numpy.typing import NDArray
 
-def finite_volume_1d(
-    u0_augmented: NDArray,
-    flux_fn: Callable[[NDArray, NDArray], NDArray],
-    dt: float,
-    dx: float,
-) -> NDArray:
+
+def lax_friedrichs_for_x(augmented_state: NDArray, 
+                         augmented_velocity_x: NDArray, 
+                         x_mesh_size:float,
+                         alpha: float) -> NDArray:
     """
-    1-D finite volume method with generic flux function.
+    Lax-Friedrichs numerical flux for the x-direction.
     
-    Solves the conservation law: u_t + f(u)_x = 0
-    using the finite volume method with forward Euler time stepping.
     
     Parameters
     ----------
-    u0_augmented : NDArray
-        Initial condition (cell averages).
-    flux_fn : Callable[[NDArray, NDArray], NDArray]
-        Numerical flux function that takes left and right states and returns fluxes.
-        Signature: flux_fn(u_left, u_right) -> fluxes at cell interfaces.
-    dt : float
-        Time step size.
-    dx : float
-        Cell width.
+    augmented_state : NDArray (3D: a, x, y). 
+                      The first and last line for x are the boundary conditions.
+    augmented_velocity_x : NDArray (2D: x, y). 
+                      The first and last line for x are the boundary conditions.
+    x_mesh_size : float
+        The mesh size in x.
+    alpha : float
+        The numerical flux parameter.
     
     Returns
     -------
-    u : NDArray
-        Solution at time t_final. NOT AUGMENTED!
-    """
-    
-        
-    # Compute fluxes at cell interfaces
-    # u[i-1/2] left state: u[i-1], u[i-1/2] right state: u[i]
-    n = len(u)
-    
-    
-    # Compute fluxes at all interfaces
-    u_left = u0_augmented[:-1]   # States to the left of interfaces
-    u_right = u0_augmented[1:]   # States to the right of interfaces
-    fluxes = flux_fn(u_left, u_right)
-    
-    # Update cell averages
-    u_new = u0_augmented[1:-1] - (dt / dx) * (fluxes[1:n+1] - fluxes[:-n])
-    
-    u = u_new
-    
-    return u
-
-
-def lax_friedrichs_flux(
-    u_left: NDArray,
-    u_right: NDArray,
-    flux_physical: Callable[[NDArray], NDArray],
-    wave_speed: float,
-) -> NDArray:
-    """
-    Lax-Friedrichs numerical flux.
-    
-    Computes the Lax-Friedrichs flux: 
-    F(u_L, u_R) = (f(u_L) + f(u_R))/2 - α(u_R - u_L)/2
-    
-    where α is an estimate of the maximum wave speed.
-    
-    Parameters
-    ----------
-    u_left : NDArray
-        Left state(s) at cell interfaces.
-    u_right : NDArray
-        Right state(s) at cell interfaces.
-    flux_physical : Callable[[NDArray], NDArray]
-        Physical flux function f(u).
-    wave_speed : float
-        Maximum wave speed (viscosity parameter).
-    
-    Returns
-    -------
-    fluxes : NDArray
+    NDArray
         Numerical fluxes at interfaces.
     """
-    f_left = flux_physical(u_left)
-    f_right = flux_physical(u_right)
-    
-    fluxes = (f_left + f_right) / 2.0 - wave_speed * (u_right - u_left) / 2.0
-    
-    return fluxes
+    first_term = augmented_velocity_x[2:, :] * augmented_state[:, 2:, :] \
+        - augmented_velocity_x[:-2, :] * augmented_state[:, :-2, :]
+    second_term = alpha * (augmented_state[:, 2:, :] -2 * augmented_state[:, 1:-1, :] 
+                           + augmented_state[:, :-2, :])
+    return (first_term - second_term) / (2*x_mesh_size)
 
-
-def upwind_flux(
-    u_left: NDArray,
-    u_right: NDArray,
-    flux_physical: Callable[[NDArray], NDArray],
-    wave_speed_fn: Callable[[NDArray], NDArray],
-) -> NDArray:
+def lax_friedrichs_for_y(augmented_state: NDArray, 
+                         augmented_velocity_y: NDArray, 
+                         y_mesh_size:float,
+                         alpha: float) -> NDArray:
     """
-    Upwind numerical flux.
+    Lax-Friedrichs numerical flux for the x-direction.
     
-    Computes the upwind flux based on the sign of the local wave speed:
-    - If a(u) > 0: F = f(u_L)  (flow from left)
-    - If a(u) < 0: F = f(u_R)  (flow from right)
-    - If a(u) = 0: F = (f(u_L) + f(u_R))/2  (no flow)
     
     Parameters
     ----------
-    u_left : NDArray
-        Left state(s) at cell interfaces.
-    u_right : NDArray
-        Right state(s) at cell interfaces.
-    flux_physical : Callable[[NDArray], NDArray]
-        Physical flux function f(u).
-    wave_speed_fn : Callable[[NDArray], NDArray]
-        Wave speed function a(u) = df/du.
+    augmented_state : NDArray (3D: a, x, y). 
+                      The first and last line for y are the boundary conditions.
+    augmented_velocity_y : NDArray (2D: x, y). 
+                      The first and last line for y are the boundary conditions.
+    y_mesh_size : float
+        The mesh size in y.
+    alpha : float
+        The numerical flux parameter.
     
     Returns
     -------
-    fluxes : NDArray
+    NDArray
         Numerical fluxes at interfaces.
     """
-    f_left = flux_physical(u_left)
-    f_right = flux_physical(u_right)
+    first_term = augmented_velocity_y[:, 2:] * augmented_state[:, :, 2:] \
+        - augmented_velocity_y[:, :-2] * augmented_state[:, :, :-2]
+    second_term = alpha * (augmented_state[:, :, 2:] -2 * augmented_state[:, :, 1:-1] 
+                           + augmented_state[:, :, :-2])
+    return (first_term - second_term) / (2*y_mesh_size)
+
+
+def upwind_for_age(augmented_state: NDArray, age_mesh_size:float) -> NDArray:
+    """
+    Upwind numerical flux for age structure.
     
-    # Compute wave speeds
-    a_left = wave_speed_fn(u_left)
-    a_right = wave_speed_fn(u_right)
+    Computes the term related to 'partial_a u':
     
-    # Average wave speed at interface
-    a_avg = (a_left + a_right) / 2.0
+    Parameters
+    ----------
+    augmented_state : NDArray (3D: a, x, y). 
+                      The first line a=0 is the boundary condition.
+    age_mesh_size : float
+        The mesh size in age strcture.
     
-    # Upwind selection
-    fluxes = np.where(a_avg > 0, f_left, np.where(a_avg < 0, f_right, (f_left + f_right) / 2.0))
+    Returns
+    -------
+    NDArray
+        Numerical term related to partial_a u.
+    """
     
-    return fluxes
+    return (augmented_state[1:, :, :] - augmented_state[:-1, :, :]) / age_mesh_size
+
+def upwind_for_x(augmented_state: NDArray, 
+                 augmented_velocity_x: NDArray, 
+                 x_mesh_size:float) -> NDArray:
+    """
+    Upwind numerical flux for x-direction.
+    
+    Computes the term related to 'partial_x (xi_x u)':
+    
+    Parameters
+    ----------
+    augmented_state : NDArray (3D: a, x, y). 
+                      The first and last line for x are the boundary conditions.
+    augmented_velocity_x : NDArray (2D: x, y). 
+                      The first and last line for x are the boundary conditions.
+    x_mesh_size : float
+        The mesh size in x.
+    
+    Returns
+    -------
+    NDArray
+        Numerical term related to partial_x (xi_x u).
+    """
+    first_term = augmented_velocity_x[2:, :] * augmented_state[:, 2:, :] \
+        - augmented_velocity_x[:-2, :] * augmented_state[:, :-2, :]
+    second_term = -0.5 * np.absolute(augmented_velocity_x[1:-1, :] + augmented_velocity_x[2:, :]) \
+        * (augmented_state[:, 2:, :] - augmented_state[:, 1:-1, :])
+    third_term = -0.5 * np.absolute(augmented_velocity_x[1:-1, :] + augmented_velocity_x[:-2, :]) \
+        * (augmented_state[:, :-2, :] - augmented_state[:, 1:-1, :])
+    
+    return (first_term + second_term + third_term) / (2*x_mesh_size)
+    
+def upwind_for_y(augmented_state: NDArray, 
+                 augmented_velocity_y: NDArray, 
+                 y_mesh_size:float) -> NDArray:
+    """
+    Upwind numerical flux for y-direction.
+    
+    Computes the term related to 'partial_y (xi_y u)':
+    
+    Parameters
+    ----------
+    augmented_state : NDArray (3D: a, x, y). 
+                      The first and last line for y are the boundary conditions.
+    augmented_velocity_y : NDArray (2D: x, y). 
+                      The first and last line for y are the boundary conditions.
+    y_mesh_size : float
+        The mesh size in y.
+    
+    Returns
+    -------
+    NDArray
+        Numerical term related to partial_y (xi_y u).
+    """
+    first_term = augmented_velocity_y[:, 2:] * augmented_state[:, :, 2:] \
+        - augmented_velocity_y[:, :-2] * augmented_state[:, :, :-2]
+    second_term = -0.5 * np.absolute(augmented_velocity_y[:, 1:-1] + augmented_velocity_y[:, 2:]) \
+        * (augmented_state[:, :, 2:] - augmented_state[:, :, 1:-1])
+    third_term = -0.5 * np.absolute(augmented_velocity_y[:, 1:-1] + augmented_velocity_y[:, :-2]) \
+        * (augmented_state[:, :, :-2] - augmented_state[:, :, 1:-1])
+    
+    return (first_term + second_term + third_term) / (2*y_mesh_size)
+    
